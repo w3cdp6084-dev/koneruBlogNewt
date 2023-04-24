e0719b7f5e0c8da5129e8fbcea54b07894a7d314
konekone <yusukew3@gmail.com>
2023-04-24 12:03:59 +0900

    newt add

---
+++ src/pages/articles/[slug].tsx
@@ -0,0 +1,45 @@
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
+import { getArticles, getArticleBySlug } from '@/libs/client'
+import type { Article } from '@/types/article'
+
+export default function Article({ article }: { article: Article }) {
+  return (
+    <>
+      <Head>
+        <title>{article.title}</title>
+        <meta name="description" content="投稿詳細ページです" />
+      </Head>
+      <main className={styles.main}>
+        <h1>{article.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: article.body }} />
+      </main>
+    </>
+  )
+}
+
+export const getStaticPaths = async () => {
+  const articles = await getArticles()
+  return {
+    paths: articles.map((article) => ({
+      params: {
+        slug: article.slug,
+      },
+    })),
+    fallback: false,
+  }
+}
+
+export const getStaticProps = async ({
+  params,
+}: {
+  params: { slug: string }
+}) => {
+  const { slug } = params
+  const article = await getArticleBySlug(slug)
+  return {
+    props: {
+      article,
+    },
+  }
+}
