1cb7096412d7e3e269f7eee3a9cd22c9d6de4842
konekone <yusukew3@gmail.com>
2023-04-25 14:34:24 +0900

    card style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,23 +14,27 @@ export default function Home({ articles, categories }: { articles: Article[], ca
         <meta name="description" content="NewtとNext.jsを利用したブログです" />
       </Head>
       <main className={styles.main}>
-        <ul>
+        <ul className={styles.cardBox}>
           {articles.map((article) => {
             const createdAt = new Date(article._sys.createdAt);
             const formattedDate = format(createdAt, 'yyyy-MM-dd');
             return (
-              <li key={article._id}>
+              <li key={article._id} className={styles.cardOuter}>
                 <Link href={`articles/${article.slug}`}>
+                    <div className={styles.cardInner}>
                     {article.coverImage && (
                       <img src={article.coverImage.src} alt={article.title} width="150" height="150" />
                     )}
+                    </div>
+                    <h3>
                     {article.title}
+                    </h3>
                     {article.categories.map((category) => (
-                      <span key={category._id}>{category.name}</span>
+                      <p key={category._id}>{category.name}</p>
                     ))}
-                    <span>{formattedDate}</span>
+                    <time>{formattedDate}</time>
                 </Link>
-                read👀
+                <p>read👀</p>
               </li>
             )
           })}
